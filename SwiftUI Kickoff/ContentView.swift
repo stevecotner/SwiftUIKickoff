022e535cc0b9452fdb4ec6bb0ce1f2e0e031c420
Steve Cotner <stevecotner@gmail.com>
2020-07-20 19:21:29 -0700

    Initial Commit

---
+++ SwiftUI Kickoff/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUI Kickoff
+//
+//  Created by Stephen E. Cotner on 7/20/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Kickoff/SwiftUI_KickoffApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_KickoffApp.swift
+//  SwiftUI Kickoff
+//
+//  Created by Stephen E. Cotner on 7/20/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_KickoffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
